ef8c7d4b83960150805f4541904a2d87a1230fd5
alfa98516 <alpha050916@gmail.com>
2026-03-07 16:41:53 +0000

    UPDATE :: added support for reading matrix from a txt file, input handling is still pretty bad

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include "Matrix.hpp"
+#include <fstream>
 #include <iostream>
 #include <string>
 int main(int argc, char* argv[]) {
 
-    if (argc != 3) {
-        fprintf(stderr, "Incorrect Usage: ./matrix [RNUM] [CNUM]");
+    if (argc != 5) {
+        fprintf(stderr, "Incorrect Usage: ./matrix [RNUM] [CNUM] [MFILE1] [MFILE2]");
         exit(1);
     }
 
     size_t row = std::stol(argv[1]);
     size_t collumn = std::stol(argv[2]);
     std::string strmat1;
-    // for (size_t i = 0; i < row; ++i) {
-    //     std::string curr;
-    //     std::cin >> curr;
-    //     strmat1 += curr;
-    // }
-    strmat1 = "0 & 0 & 0 & 1 \\ 0 & 1 & 0 & 0 \\ 0 & 0 & 1 & 0 \\ 1 & 0 & 0 & 0 \\";
 
+    std::ifstream f(argv[3]);
+    std::string line;
+    while (std::getline(f, line)) {
+        strmat1 += line;
+    }
+
+    std::cout << strmat1;
     std::string strmat2;
+    std::ifstream f2(argv[3]);
 
-    // for (size_t j = 0; j < row; ++j) {
-    //     std::string curr;
-    //     std::cin >> curr;
-    //     strmat2 += curr;
-    // }
+    while (std::getline(f2, line)) {
+        strmat2 += line;
+    }
 
-    strmat2 = "0 & 0 & 5 & -5 \\ -2 & 1 & -2 & -1 \\ 0 & 4 & 3 & -1 \\ 2 & 1 & 3 & 1";
     Matrix mat1 = Matrix(strmat1, row, collumn);
     Matrix mat2 = Matrix(strmat2, row, collumn);
     std::string result = mat1 * mat2;
-    std::cout << result;
+    std::cout << result << '\n';
 }
